52ad23427e4c04d98e27d91357d4b553080915e5
Madhushree Gupta <madhushree25@gmail.com>
2020-11-21 16:03:08 +0000

    files added

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './app/screens/HomeScreen';
+import ProfileScreen from './app/screens/ProfileScreen';
+import DetailsScreen from './app/screens/DetailsScreen';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    const getFonts = async() => {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        ...Ionicons.font,
+      });
+    }
+  getFonts();
+  setIsReady(true);
+
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
